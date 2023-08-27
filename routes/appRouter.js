c912c1420145ddc816d2211f036a00b6db3118b7
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-27 17:23:39 +0530

    backend task-3 api for monthly stats

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -3,8 +3,11 @@ const router = Router();
 
 import {getPopulate} from '../controllers/appController.js'
 import { getAllData } from "../controllers/searchController.js";
+import { getStats } from "../controllers/statsController.js";
+
 
 router.get("/populate", getPopulate)
 router.get("/", getAllData)
+router.get("/stats", getStats)
 
 export default router;
