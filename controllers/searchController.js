dfb9f3489e6b9868241add747f70c65cfbf807b9
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-27 16:32:10 +0530

    backend task-2 added pagination

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -2,6 +2,7 @@ import App from "../model/AppModel.js";
 
 export const getAllData = async (req, res) => {
   try {
+    //search
     const { search } = req.query;
     let query = {};
     if (search) {
@@ -13,9 +14,17 @@ export const getAllData = async (req, res) => {
         ],
       };
     }
-    const data = await App.find(query);
+    //pagination
+     const page = Number(req.query.page) || 1;
+     const limit = 10;
+     const skip = (page - 1) * limit;
+
+
+    const data = await App.find(query).skip(skip).limit(limit);
     const total = await App.countDocuments(query);
-    res.status(200).json({ total, data });
+    const numOfPages = Math.ceil(total / limit);
+
+    res.status(200).json({ total, numOfPages, currentPage: page, data });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
