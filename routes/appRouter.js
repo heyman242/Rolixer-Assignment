0cc9b38284d4f7b97f8611813495a9025d77fc2b
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-27 18:49:16 +0530

    backend task-5 task-6 added pie chart and combined data api

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -5,10 +5,14 @@ import { getPopulate } from "../controllers/appController.js";
 import { getAllData } from "../controllers/searchController.js";
 import { getStats } from "../controllers/statsController.js";
 import { getBarChart } from "../controllers/barChartController.js";
+import { getPieChart } from "../controllers/pieChartController.js";
+import {getCombineData} from "../controllers/combineDataController.js"
 
 router.get("/populate", getPopulate);
 router.get("/", getAllData);
 router.get("/stats", getStats);
 router.get("/barChart", getBarChart);
+router.get("/pieChart", getPieChart);
+router.get("/combineData", getCombineData)
 
 export default router;
