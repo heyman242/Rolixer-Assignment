984109abe9e434bf35a6d1208aaf3943d5780bd6
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-27 18:00:13 +0530

    backend task-4 added api for bar chart

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 const router = Router();
 
-import {getPopulate} from '../controllers/appController.js'
+import { getPopulate } from "../controllers/appController.js";
 import { getAllData } from "../controllers/searchController.js";
 import { getStats } from "../controllers/statsController.js";
+import { getBarChart } from "../controllers/barChartController.js";
 
-
-router.get("/populate", getPopulate)
-router.get("/", getAllData)
-router.get("/stats", getStats)
+router.get("/populate", getPopulate);
+router.get("/", getAllData);
+router.get("/stats", getStats);
+router.get("/barChart", getBarChart);
 
 export default router;
